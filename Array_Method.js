460fe7b34b0ed741b5fee624a8345bbed329e3d1
ashikulislamcse <ashikulislamcse@gmail.com>
2025-03-02 23:19:59 +0600

    Array Methods

--- Array_Method.js
+++ Array_Method.js
@@ -1,6 +1,6 @@
 
 
-// map function
+// map Method
 
 const number = [1,3,5,7,9];
 
@@ -10,3 +10,11 @@ const double = number.map((number)=>{
 console.log(double);
 
 
+//Filter Method
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+
+const evenNumber  = numbers.filter((number)=>{
+    return (number%2 ===0);
+});
+console.log(evenNumber);
