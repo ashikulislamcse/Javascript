4baefcd2c1d0f17718f6f290485c7f1843e60b72
ashikulislamcse <ashikulislamcse@gmail.com>
2025-03-03 05:08:08 +0600

    Updated

--- Array_Method.js
+++ Array_Method.js
@@ -2,9 +2,9 @@
 
 // map Method
 
-const number = [1,3,5,7,9];
+const numbers1 = [1,3,5,7,9];
 
-const double = number.map((number)=>{
+const double = numbers1.map((number)=>{
    return (number*2);
 });
 console.log(double);
@@ -12,9 +12,18 @@ console.log(double);
 
 //Filter Method
 
-const numbers = [1,2,3,4,5,6,7,8,9];
+const numbers2 = [1,2,3,4,5,6,7,8,9];
 
-const evenNumber  = numbers.filter((number)=>{
+const evenNumber  = numbers2.filter((number)=>{
     return (number%2 ===0);
 });
 console.log(evenNumber);
+
+
+
+//reduce() Method
+
+const numbers3 = [1,2,3,4,5];
+
+const sum = numbers3.reduce((x,y)=> x+y,0)
+console.log(sum)
