4324f28163339434f6fdf564b890818885783834
ashikulislamcse <ashikulislamcse@gmail.com>
2025-03-24 21:52:49 +0600

    Updated

--- object.js
+++ object.js
@@ -13,7 +13,9 @@ console.log(person.isStudent);
 
 const Students = {
     name:"Ashikul",
-    roll:"600817"
+    roll:"600817",
+    dep:"Computer Science and Technology"
 }
 console.log(Students.name);
 console.log(Students.roll);
+console.log(Students.dep);
